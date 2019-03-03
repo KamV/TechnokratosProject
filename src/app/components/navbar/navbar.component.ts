f91d8b3f0c4db31ad6154e934fe2c4f39bfb3348
Kamil <valiullin2716@gmail.com>
2019-03-03 23:42:13 +0300

    Fix navbar collapsing

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.styl']
+  styleUrls: ['./navbar.component.styl'],
+  animations: [
+    trigger("collapseAnimation", [
+         state('collapsed', style({
+            height: '0px',
+            overflow: 'hidden'
+         })),
+         state('expanded', style({
+            height: '*'
+         })),
+
+         transition('collapsed <=> expanded', animate('700ms ease-in'))
+      ])
+  ]
 })
 
 export class NavBarComponent implements OnInit {
 
+  state: string = 'collapsed';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleCollapse() {
+    this.state = this.state === 'collapsed' ? 'expanded' : 'collapsed';
   }
 
 }
